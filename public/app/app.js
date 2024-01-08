962cd707a02e0a65d7882300b5f8ba531e80d822
Guilherme Andre de Melo <guilhermeanm95@hotmail.com>
2024-01-08 18:47:15 -0300

    Aplicando filter, map e reduce

--- public/app/app.js
+++ public/app/app.js
@@ -1,9 +1,13 @@
-import { handlerStatus } from "./utils/promise-helpers.js";
+import { handlerStatus, log } from "./utils/promise-helpers.js";
 
 document
 .querySelector('#myButton')
 .onclick = () => 
     fetch('http://localhost:3000/notas')
     .then(handlerStatus)
-    .then(notas => console.log(notas))
+    .then(notas => notas.reduce((array, nota) => array.concat(nota.itens), []))
+    .then(log)
+    .then(itens => itens.filter(item => item.codigo == '2143'))
+    .then(itens => itens.reduce((total, item) => total + item.valor, 0))
+    .then(console.log)
     .catch(console.log)
