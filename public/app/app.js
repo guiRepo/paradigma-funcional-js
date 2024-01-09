90c8e52b1279f6bdefffee879be7dc47d7de9b67
Guilherme Andre de Melo <guilhermeanm95@hotmail.com>
2024-01-09 16:15:40 -0300

    criação do takeutils para setar quantidade de vezes que o serviço é acionado

--- public/app/app.js
+++ public/app/app.js
@@ -1,13 +1,18 @@
 import { log } from "./utils/promise-helpers.js";
 import { notasService as service } from "./nota/service.js";
 import "./utils/array-helpers.js";
+import { takeUntil } from "./utils/operators.js";
 
 
-
-document.querySelector("#myButton").onclick = () =>
+const operation = takeUntil (3, () => 
   service
     .sumItems("2143")
     .then(console.log)
-    .catch(console.log);
+    .catch(console.log)
+)
+document
+  .querySelector("#myButton")
+  .onclick = operation
+
 
 // Functor => Todo aquele que pode ser realizado um map (Array)
--- public/app/utils/operators.js
+++ public/app/utils/operators.js
@@ -7,6 +7,9 @@ export const compose = (...fns) => value =>
 export const pipe = (...fns) => value => 
     fns.reduce((previousValue, fn)=> fn(previousValue), value)
 
+export const takeUntil = (times, fn) => 
+    () => times-- > 0 && fn()
+
 
 //reduceRight - faz a varredura do reduce da direita pra esquerda
 //reduce - faz a varredura da esquerda pra direita
